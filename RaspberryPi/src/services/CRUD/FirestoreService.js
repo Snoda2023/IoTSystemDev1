d9226bdbbc25ca1917de0a0d3aaf106ecdbf6005
Snoda2023 <tk220137syotaro.noda@gmail.com>
2023-07-18 16:36:16 +0900

    FirestoreService fix

--- RaspberryPi/src/services/CRUD/FirestoreService.js
+++ RaspberryPi/src/services/CRUD/FirestoreService.js
@@ -1,37 +1,22 @@
-require('date-utils');  // 現在日時を取得するライブラリ
-const admin = require('firebase-admin');
-const serviceAccount = require('PATH/TO/JSON/FILE');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const serviceAccount = require('/home/tk220223/IoTSystemDev1/RaspberryPi/serviceAccountKey.json');
+initializeApp({
+  credential: cert(serviceAccount)
 });
 
 class FirestoreService {
-  // Firestore へのデータの書き込み
-  static async writeDataToFirestore(is_using, peopleCount, congestionDegree) {
-    const db = admin.firestore();
-
-    // 現在時刻を取得
-    const date = new Date();
-    const formattedDate = date.toFormat('YYYYMMDDHH24MISS');
-
-    // Firestore へ書き込み
-    await db
-      .collection('documents')
-      .doc('Xcnsg7BANmgmfn4e86YT')
-      .collection('device1')
-      .add({
-        timestamp: formattedDate, // 時間
-        peopleCount: peopleCount, // 人数
-        congestionDegree: congestionDegree, // 混雑率
-        room_number: "354",       // 部屋番号
-        is_using: is_using,       // 使用中か
-      })
-      .then((res) => {
-        return res;
-      })
-      .catch((err) => {
-        console.log('Error: ', err);
-      });
+  static async writeDataToFirestore(peopleCount, congestionDegree) {
+    const db = getFirestore();
+    const data = {
+      peopleCount: peopleCount,      // 検出人数
+      crowded_lvl: congestionDegree, // 混雑度
+      is_using: false,               // 使用中か
+      room: "251",                   // 部屋番号
+      createdAt: firebase.firestore.FieldValue.Timestamp() // タイムスタンプ
+    };
+    const res = await db.collection('rooms').set(data);
   }
 }
+
 module.exports = FirestoreService;
